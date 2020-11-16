806c4d46e3ed1e37543ebd3b86bf6b080da6cfdd
erenkucuk <erenkkucuk89@gmail.com>
2020-11-15 21:00:33 -1000

    added new things to remember that

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,7 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Helloo");
+
+        System.out.println("I dont wanna forget it so I need more practise");
     }
 }
